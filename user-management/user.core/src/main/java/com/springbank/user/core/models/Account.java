e52fea6e1d177f5d5f12f361503bf7073e1eb1fd
dogaanismail <ismaildogaan@gmail.com>
2022-05-27 15:48:14 +0300

    SpringBankNet - EventHandlers, rest api controller and validations for models for user query api service have been added.

--- user-management/user.core/src/main/java/com/springbank/user/core/models/Account.java
+++ user-management/user.core/src/main/java/com/springbank/user/core/models/Account.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -12,7 +14,13 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class Account {
+
+    @Size(min = 2, message = "userName must have a minimum of 2 characters!")
     private String userName;
+
+    @Size(min = 7, message = "userName must have a minimum of 7 characters!")
     private String password;
+
+    @NotNull(message = "specify at least 1 user role!")
     private List<Role> roles;
 }
